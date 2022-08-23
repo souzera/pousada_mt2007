17bbec0532d26a6f73e4d1bac2f10098acf2da60
Matheus Barbosa <ps.matheusb@gmail.com>
2022-08-23 15:49:32 -0300

    add index route

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.register_blueprint()
+#app.register_blueprint()
+
+@app.route('/')
+def index():
+    return "<div style='display:flex; justify-content:center;align-items:center; box-sizing: border-box; wight: 40vh; height:40vw'>" \
+           "<h1 style='font-family:sans-serif;'>Olá mundo</h1>" \
+           "</div>"
+
+app.run()
