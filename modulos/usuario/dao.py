eb9be7e7f8e200f4e101ae8479d1f553daea5d26
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-05 15:56:22 -0300

    get all usuarios

--- modulos/usuario/dao.py
+++ modulos/usuario/dao.py
@@ -1,4 +1,6 @@
 from database.connect import ConnectDataBase
+from modulos.usuario.usuario import Usuario
+
 
 class UsuarioDAO:
     _TABLE_NAME = 'usuario'
@@ -7,13 +9,13 @@ class UsuarioDAO:
     _SELECT_ALL = f'SELECT * FROM {_TABLE_NAME}'
     _SELECT_BY_ID = 'SELECT * FROM {} WHERE ID={}'
     _SELECT_BY_USERNAME = "SELECT * FROM {} WHERE username ILIKE '{}'"
+    _SELECT_BY_STATUS = "SELECT * FROM {} WHERE status ILIKE '{}'"
 
     def __init__(self):
         self.database = ConnectDataBase().get_instance()
 
     # TODO implement
         # GET_ALL
-        # SAVE
         # EXCLUDE
         # UPDATE
 
@@ -31,5 +33,16 @@ class UsuarioDAO:
 
 
     def get_all(self):
-        pass
+        usuarios = []
+        cursor = self.database.cursor()
+        cursor.execute(self._SELECT_ALL)
+        all_usuarios = cursor.fetchall()
+        colluns_name = [desc[0] for desc in cursor.description]
+        print(colluns_name)
+        for usuario_query in all_usuarios:
+            data = dict(zip(colluns_name, usuario_query))
+            usuario = Usuario(**data)
+            usuarios.append(usuario)
+        cursor.close()
+        return usuarios
 
