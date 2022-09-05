f3ec7567c1f7f895d1e528f19b57dee0eaf76c1b
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-05 15:55:52 -0300

    get all reserva

--- modulos/reserva/dao.py
+++ modulos/reserva/dao.py
@@ -1,4 +1,6 @@
 from database.connect import ConnectDataBase
+from modulos.reserva.reserva import Reserva
+
 
 class ReservaDAO:
 
@@ -6,9 +8,10 @@ class ReservaDAO:
 
     _INSERT_INTO = f'INSERT INTO {_TABLE_NAME}(cliente_id, comodo_id, status) VALUES(%s, %s, %s) RETURNING id'
     _SELECT_ALL = f'SELECT * FROM {_TABLE_NAME}'
-    _SELECT_BY_ID = 'SELECT * FROM {} WHERE ID={}'
-    _SELECT_BY_CLIENTE = "SELECT * FROM {} WHERE cliente_id ILIKE '{}'"
-    _SELECT_BY_COMODO = "SELECT * FROM {} WHERE comodo_id ILIKE '{}'"
+    _SELECT_BY_ID = f'SELECT * FROM {_TABLE_NAME} WHERE ID=%d'
+    _SELECT_BY_CLIENTE = f"SELECT * FROM {_TABLE_NAME} WHERE cliente_id ILIKE %s"
+    _SELECT_BY_COMODO = f"SELECT * FROM {_TABLE_NAME} WHERE comodo_id ILIKE %s"
+    _SELECT_BY_STATUS = f"SELECT * FROM {_TABLE_NAME} WHERE status ILIKE %s"
 
     def __init__(self):
         self.database = ConnectDataBase().get_instance()
@@ -29,3 +32,16 @@ class ReservaDAO:
             return reserva
         else:
             raise Exception('Não foi possivel salvar')
+
+    def get_all(self):
+        reservas = []
+        cursor = self.database.cursor()
+        cursor.execute(self._SELECT_ALL)
+        all_reservas = cursor.fetchall()
+        colluns_name = [desc[0] for desc in cursor.description]
+        for reserva_query in all_reservas:
+            data = dict(zip(colluns_name, reserva_query))
+            reserva = Reserva(**data)
+            reservas.append(reserva)
+        cursor.close()
+        return reserva
