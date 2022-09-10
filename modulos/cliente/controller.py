8d5fec819974d2ef74f242991f83c1de7b78fe24
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-10 19:23:18 -0300

    add functions of cliente controller

--- modulos/cliente/controller.py
+++ modulos/cliente/controller.py
@@ -2,7 +2,7 @@ from flask import jsonify, make_response, Blueprint
 
 from modulos.cliente.dao import ClienteDAO
 
-app_cliente = Blueprint('example_blueprint', __name__)
+app_cliente = Blueprint('cliente_blueprint', __name__)
 app_name = 'cliente'
 dao_cliente = ClienteDAO()
 
@@ -10,4 +10,16 @@ dao_cliente = ClienteDAO()
 def get_clientes():
     clientes = dao_cliente.get_all()
     data = [cliente.get_data_dict() for cliente in clientes]
+    return make_response(jsonify(data))
+
+@app_cliente.route(f'/{app_name}/add/', methods=['POST'])
+def save_cliente(cliente):
+    cliente = dao_cliente.salvar(cliente)
+    data = cliente.get_data_dict()
+    return make_response(jsonify(data))
+
+@app_cliente.route(f'/{app_name}/<id>', methods=['GET'])
+def get_id(id):
+    cliente = dao_cliente.get_by_id(id)
+    data = cliente.get_data_dict()
     return make_response(jsonify(data))
--- modulos/cliente/dao.py
+++ modulos/cliente/dao.py
@@ -10,7 +10,7 @@ class ClienteDAO:
     _INSERT_INTO = f'INSERT INTO {_TABLE_NAME}(nome, cpf, telefone,' \
                    f' dtNascimento, endereco, sexo) VALUES(%s, %s, %s, %s, %s, %s) RETURNING id'
     _SELECT_ALL = f'SELECT * FROM {_TABLE_NAME}'
-    _SELECT_BY_ID = 'SELECT * FROM {} WHERE ID={}'
+    _SELECT_BY_ID = f'SELECT * FROM {_TABLE_NAME} WHERE ID=%s'
     _SELECT_BY_CPF = "SELECT * FROM {} WHERE cpf ILIKE '{}'"
 
     def __init__(self):
@@ -46,6 +46,12 @@ class ClienteDAO:
         cursor.close()
         return clientes
 
-    def to_url(self, value):
-        if isinstance(value, date):
-            return value.strftime('%Y-%m-%d')
+    def get_by_id(self, id):
+        cursor = self.database.cursor()
+        cursor.execute(self._SELECT_BY_ID, id)
+        coluns_name = [desc[0] for desc in cursor.description]
+        cliente_query = cursor.fetchone()
+        data = dict(zip(coluns_name, cliente_query))
+        cliente = Cliente(**data)
+        cursor.close()
+        return cliente
