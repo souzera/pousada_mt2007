86b0f5815d07d69db9951e7446411ce2510a96e2
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-10 19:44:26 -0300

    add checkin and checkout to reserva

--- modulos/reserva/reserva.py
+++ modulos/reserva/reserva.py
@@ -1,7 +1,9 @@
 class Reserva:
 
-    def __init__(self, cliente, comodo, status, id=None):
+    def __init__(self, checkin, checkout, cliente, comodo, status, id=None):
         self.id = id
+        self.checkin = checkin
+        self.checkout = checkout
         self.cliente = cliente
         self.comodo = comodo
         self.status = status
@@ -12,6 +14,8 @@ class Reserva:
 
     def get_data_dict(self):
         return {'id': self.id,
+                'checkin': self.checkin,
+                'checkout': self.checkout,
                 'cliente': self.cliente.get_data_dict(),
                 'comodo': self.comodo.get_data_dict(),
                 'status': self.status}
