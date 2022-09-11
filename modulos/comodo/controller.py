b9fdf11fc36632bf2d3a335b77e72017760474f8
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-11 18:02:29 -0300

    implement routes

--- modulos/comodo/controller.py
+++ modulos/comodo/controller.py
@@ -1,13 +1,28 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, request
 
+from modulos.comodo.comodo import Comodo
 from modulos.comodo.dao import ComodoDAO
 
 app_comodo = Blueprint('comodo_blueprint', __name__)
 app_name = 'comodo'
 dao_comodo = ComodoDAO()
 
-@_app.route(f'/{app_name}/', methods=['GET'])
+@app_comodo.route(f'/{app_name}/', methods=['GET'])
 def get_comodos():
     comodos = dao_comodo.get_all()
     data = [comodo.get_data_dict() for comodo in comodos]
+    return make_response(jsonify(data))
+
+@app_comodo.route(f'/{app_name}/add/', methods=['POST'])
+def save_cliente():
+    data = request.get_json()
+    comodo = Comodo(**data)
+    if dao_comodo.salvar(comodo):
+        return make_response(jsonify(data))
+    return 404
+
+@app_comodo.route(f'/{app_name}/<id>', methods=['GET'])
+def get_id(id):
+    comodo = dao_comodo.get_by_id(id)
+    data = comodo.get_data_dict()
     return make_response(jsonify(data))
